a7ca05390c30f488299faa1b9e9fef91b03727e7
Spenser <scru7556@vandals.uidaho.edu>
2023-11-02 14:12:19 -0700

    added a linear move function

---
+++ DeltaArm/InvKin.h
@@ -0,0 +1,7 @@
+#ifndef INVKIN_H
+#define INVKIN_H
+
+int delta_calcAngleYZ(float x0, float y0, float z0, float& theta);
+int delta_calcInverse(float x0, float y0, float z0, float& theta1, float& theta2, float& theta3);
+
+#endif
