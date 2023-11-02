5b488f37f1a67dbdca4abbfd3ff4dc320e8cf862
Spenser <scru7556@vandals.uidaho.edu>
2023-11-02 14:25:38 -0700

    fixed git issue

--- DeltaArm/InvKin.h
+++ DeltaArm/InvKin.h
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 #ifndef INVKIN_H
 #define INVKIN_H
 
 int delta_calcAngleYZ(float x0, float y0, float z0, float& theta);
 int delta_calcInverse(float x0, float y0, float z0, float& theta1, float& theta2, float& theta3);
 
-=======
-#ifndef INVKIN_H
-#define INVKIN_H
-
-int delta_calcAngleYZ(float x0, float y0, float z0, float& theta);
-int delta_calcInverse(float x0, float y0, float z0, float& theta1, float& theta2, float& theta3);
-
->>>>>>> 8cc71c1f1aa978e205888081b732ee4624109bca
 #endif
--- DeltaArm/LinearPath.h
+++ DeltaArm/LinearPath.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef LINEARPATH_H
 #define LINEARPATH_H
 
@@ -10,16 +9,4 @@ void currentPos(float x, float y, float z, float cPos[3]);
 void moveMJT(int duration_in_milliseconds, float cPos[], float fPos[], Servo s1, Servo s2, Servo s3);
 void move(int duration, float cPos[], float fPos[], Servo s1, Servo s2, Servo s3);
 
-=======
-#ifndef LINEARPATH_H
-#define LINEARPATH_H
-
-#include <Servo.h>
-#include <math.h>
-#include <Arduino.h>
-
-void currentPos(float x, float y, float z, float cPos[3]);
-void moveMJT(int duration_in_milliseconds, float cPos[], float fPos[], Servo s1, Servo s2, Servo s3);
-
->>>>>>> 8cc71c1f1aa978e205888081b732ee4624109bca
 #endif
